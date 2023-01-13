a68ad2eb9db433477fda5e26fb3a758cb1977fd0
chris-wamae <chriswamae123@gmail.com>
2023-01-13 17:47:11 +0300

    created a useEffect that fetches an image URL

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,13 @@
 // create your App component here
+import React,{useEffect} from "react"
+function App(){
+    useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((r) => r.json())
+    .then((d) => console.log(d))
+    })
+    return(
+    <></>
+    )
+}
+export default App
